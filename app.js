df0d39f920544579194e3ab4a764377df63be916
Drake Scott <ArbiterBibendi@yahoo.com>
2025-06-04 21:25:02 -0500

    enable cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var sessionsRouter = require('./routes/sessions');
 var eventsRouter = require('./routes/events');
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(cors());
 app.use('/sessions', sessionsRouter);
 app.use('/events', eventsRouter);
 
