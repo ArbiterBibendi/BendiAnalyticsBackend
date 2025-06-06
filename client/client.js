96a9257fdde32cdab6cf51023fa17d729fb4ccc4
Drake Scott <ArbiterBibendi@yahoo.com>
2025-06-05 20:56:25 -0500

    add events to clientscript, add edata, and fix event table

--- client/client.js
+++ client/client.js
@@ -2,10 +2,36 @@ const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
 const url = document.currentScript.src;
 const baseUrl = new URL(url).origin;
+
+/* Send session data to sessions endpoint */
 fetch(`${baseUrl}/sessions`, {
 	method: 'POST',
 	body: JSON.stringify({ s: searchParams.get("s") }),
 	headers: {
 		'Content-Type': 'application/json'
 	}
+});
+
+/* Set all elements on page to send event data to events endpoint */
+const onClick = (e) => {
+	const etype = e.type;
+	const edataextra = e.target.href ? e.target.href : e.target.innerText;
+	const edata = `${window.location}:${e.target.tagName}:${edataextra}`
+	fetch(`${baseUrl}/events`, {
+		method: 'POST',
+		body: JSON.stringify({ et: etype, ed: edata }),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+};
+
+/* Click events */
+['button', 'a'].forEach(selector => {
+	document.querySelectorAll(selector).forEach((element) => {
+		element.addEventListener("click", onClick);
+	});
+	document.querySelectorAll(selector).forEach((element) => {
+		element.addEventListener("auxclick", onClick);
+	});
 });
--- routes/events.js
+++ routes/events.js
@@ -5,15 +5,18 @@ const { v4 } = require('uuid');
 
 
 /* POST events. */
+/* et: etype, ed: edata */
 router.post('/', async (req, res, next) => {
   const uuid = v4();
-  const etype = req.body.e ? req.body.e : "none";
+  const etype = req.body.et ? req.body.et : "none";
+  const edata = req.body.ed ? req.body.ed : "none";
   const clientip = req.headers['x-forwarded-for'] || req.ip;
   try {
-    await pool.query("INSERT INTO events (id, etype, ip) VALUES ($1, $2, $3)", [uuid, etype, clientip]);
+  await pool.query("INSERT INTO events (id, etype, edata, ip) VALUES ($1, $2, $3, $4);", [uuid, etype, edata, clientip]);
   } catch (e) {
     console.error(e);
     res.status(418).send(e.message);
+    return;
   }
   res.send("Success");
 });
