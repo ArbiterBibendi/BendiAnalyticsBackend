f987c73970ea8a4689b65f0c9aa6d1fe81bff4c4
Drake Scott <ArbiterBibendi@yahoo.com>
2025-06-06 16:08:26 -0500

    update pool to use db name

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const pool = new Pool({
 	host: process.env.POSTGRES_HOST,
 	port: process.env.POSTGRES_PORT,
 	user: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB
 });
 
 module.exports = pool;
